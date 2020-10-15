26bcddbc27fa1b55342c0c91a45dd44bff45a01e
Lorenze Jay <Lorenzejay@gmail.com>
2020-10-14 19:29:47 -0700

    Updates

---
+++ precache-manifest.26673e0230c8e2c8c3cfe28e092cbc97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613b08ebe0d6d128bb8dfbc6637a590e",
+    "url": "/ljaph-minimanote/index.html"
+  },
+  {
+    "revision": "27ee0f07ff2c42cf071a",
+    "url": "/ljaph-minimanote/static/css/main.6c98db66.chunk.css"
+  },
+  {
+    "revision": "011723a8afa04d52a082",
+    "url": "/ljaph-minimanote/static/js/2.7a1447ef.chunk.js"
+  },
+  {
+    "revision": "b36dc62eee4575fad7c7f81ae1187ccc",
+    "url": "/ljaph-minimanote/static/js/2.7a1447ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ee0f07ff2c42cf071a",
+    "url": "/ljaph-minimanote/static/js/main.14d5f790.chunk.js"
+  },
+  {
+    "revision": "7963bb5e01f3d48eb9ce",
+    "url": "/ljaph-minimanote/static/js/runtime-main.b0210007.js"
+  },
+  {
+    "revision": "250b9ed6ad4bf1f49462a0212fe23b25",
+    "url": "/ljaph-minimanote/static/media/stephan-seeber-PzqrVrfjasc-unsplash.250b9ed6.jpg"
+  }
+]);
